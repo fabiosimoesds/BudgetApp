03566c420cbed505e44d3e4fa70720c35bfbbfe0
FABIO SANTOS <98475783+fabiosimoesds@users.noreply.github.com>
2022-03-23 11:19:10 +0000

    Final Commit
    Project finalized and tested on the FreeCodeCamp, No Erros founded by their test machine.

--- budget.py
+++ budget.py
@@ -22,6 +22,8 @@ class Category:
             withd['description'] = description
             self.ledger.append(withd)
             return True
+        else:
+            return False
 
     def get_balance(self):
         sum_values = 0
@@ -36,13 +38,15 @@ class Category:
         if check is True:
             trans = dict()
             trans['amount'] = amount
-            trans['description'] = f'Transfer from {self.category}'
+            trans['description'] = f'Transfer from {self.category.title()}'
             another_category.ledger.append(trans)
             trans_wit = dict()
             trans_wit['amount'] = amount*-1
-            trans_wit['description'] = f'Transfer to {another_category.category}'
+            trans_wit['description'] = f'Transfer to {another_category.category.title()}'
             self.ledger.append(trans_wit)
             return True
+        else:
+            return False
 
 
 
@@ -56,20 +60,77 @@ class Category:
             return True
 
     def format(self):
-        print(f'{self.category.title():*^30}')
-        for value in self.ledger:
-            amount = str(f'{value["amount"]:.2f}')
-            print(f'{value["description"][:23]:<23}{amount:>7}')
+        listToConcat = list()
+        listToConcat.append(f'{self.category.title():*^30}')
         sum_values = 0
         for value in self.ledger:
             sum_values += value['amount']
-        print(f'Total: {sum_values}')
+        for value in self.ledger:
+            amount = str(f'{value["amount"]:.2f}')
+            listToConcat.append(f'{value["description"][:23]:<23}{amount:>7}')
+        listToConcat.append(f'Total: {sum_values}')
+        prettyprint = ''
+        for v in listToConcat:
+            if 'Total' in v:
+                prettyprint += v
+            else:
+                prettyprint += v+'\n'
+        return prettyprint
+
 
     def __str__(self):
         return str(Category.format(self))
 
 
 def create_spend_chart(categories):
-    print('hi')
+    dic = dict()
+    cat_name = list()
+    for values in categories:
+        tot = 0
+        list_led = values.ledger
+        cat = values.category.title()
+        cat_name.append(cat)
+        for index, values in enumerate(list_led):
+            if list_led[index]['amount'] < 0:
+                tot += float(list_led[index]['amount'])
+                dic[cat] = tot
+    total_spent = 0
+    for keys, values in dic.items():
+        total_spent += values
+    dic_perc = dict()
+    for keys, values in dic.items():
+        dic_perc[keys] = int(values/total_spent*100//10*10)
+    format_list = list()
+    percent_list2 = list()
+    format_list.insert(0, 'Percentage spent by category\n')
+    for a in range(100, -10, -10):
+        for keys, values in dic_perc.items():
+            if values >= a:
+                percent_list2.append('o  ')
+            else:
+                percent_list2.append('   ')
+        format_list.append(f'{str(a) + "|":>4} {"".join(percent_list2[:])}\n')
+        percent_list2.clear()
+
+    print()
+    format_list.append(f'    {"-"*3*len(dic_perc)}-\n')
+
+    max_leng = max(map(len, cat_name))
+    padded_cat = [item.ljust(max_leng, ' ')for item in cat_name]
+    count = 0
+    for row in zip(*padded_cat):
+        if count < max_leng-1:
+            format_list.append('     ')
+            format_list.append(f'{"  ".join(row)}  \n')
+            count += 1
+        else:
+            format_list.append('     ')
+            format_list.append(f'{"  ".join(row)}  ')
+    result_formated = ''.join(format_list)
+    return result_formated
+
+
+
+
 
 
